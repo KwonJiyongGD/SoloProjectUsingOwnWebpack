f9d094b9dda8d82f2d85a7434332c380864cd79b
Michael Liu <micliu@ucdavis.edu>
2023-06-06 12:40:49 -0700

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import ProfileContainer from './components/ProfileContainer';
 import CreateProfileForm from './components/CreateProfileForm';
 import PictureShower from './components/RandomAPIpic';
 import fetchRandomImage from './module/api';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="app">
       <h1 style={{ color: 'red' }}>Michael/Kwon's CRUD(dy) App</h1>
+      <Navbar />
       <CreateProfileForm />
       <ProfileContainer />
       {console.log(fetchRandomImage())}
