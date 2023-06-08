370a7f5bb0860afe0aea8cf4e5cbe0cf1c49c7cd
Michael Liu <micliu@ucdavis.edu>
2023-06-08 15:17:21 -0700

    POST APC SUCCESS

--- src/backend/controllers/ProfileController.js
+++ src/backend/controllers/ProfileController.js
@@ -1,62 +1,92 @@
-const ProfileModel = require('../models/ProfileModel');
+const Profile = require('../models/ProfileModel');
+// const fetch = require('node-fetch');
 
-module.exports.getProfile = async (req, res) => {
-  try {
-    const profiles = await ProfileModel.find();
-    res.send(profiles);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error retrieving profiles');
-  }
+const ProfileController = {
+  getProfile(req, res, next) {
+    const { name } = req.body;
+
+    Profile.findOne({ name: name })
+      .then((selected) => {
+        if (!selected) {
+          return next({
+            log: 'Error in finding selected name in getProfile',
+            status: 400,
+            message: 'Error in finding profile',
+          });
+        }
+        res.locals.selected = selected;
+        return next();
+      })
+      .catch((err) => {
+        return next({
+          log: 'Error in profile getProfile',
+          status: 400,
+          message: 'Error in finding profile',
+        });
+      });
+  },
+
+  updateProfile(req, res, next) {
+    const { name } = req.params;
+    const { age } = req.body;
+
+    Profile.findOneAndUpdate({ name: name }, { age }, { new: true })
+      .then((selectedProfile) => {
+        res.locals.selectedProfile = selectedProfile;
+        return next();
+      })
+      .catch((err) => {
+        return next({
+          log: 'Error updating profile :(',
+          status: 400,
+          message: 'Error in updating profile',
+        });
+      });
+  },
+
+  deleteProfile(req, res, next) {
+    const { name } = req.body;
+
+    Profile.findOneAndDelete({ name: name }, { age }, { new: true })
+      .then((selectedProfile) => {
+        res.locals.selectedProfile = selectedProfile;
+        return next();
+      })
+      .catch((err) => {
+        return next({
+          log: 'Error in deleting profile :(',
+          status: 400,
+          message: 'Error in deleting profile',
+        });
+      });
+  },
+
+  async saveProfile(req, res) {
+    try {
+      const { name, age, gender, info } = req.body;
+
+      // Fetch a random dog image
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      const imageUrl = data.message;
+
+      const profile = new Profile({
+        name,
+        age,
+        gender,
+        info,
+        imageUrl,
+      });
+
+      const savedProfile = await profile.save();
+
+      // Send the saved profile back to the frontend
+      res.json(savedProfile);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error saving profile');
+    }
+  },
 };
 
-module.exports.saveProfile = async (req, res) => {
-  try {
-    const { name, age, gender, info } = req.body;
-
-    // Fetch a random dog image
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
-    const data = await response.json();
-    const imageUrl = data.message;
-
-    const profile = new ProfileModel({
-      name,
-      age,
-      gender,
-      info,
-      imageUrl,
-    });
-
-    const savedProfile = await profile.save();
-
-    // Send the saved profile back to the frontend
-    res.json(savedProfile);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error saving profile');
-  }
-};
-
-module.exports.updateProfile = async (req, res) => {
-  try {
-    const { _id, name, age, gender, info } = req.body;
-
-    await ProfileModel.findByIdAndUpdate(_id, { name, age, gender, info });
-    res.send('Updated the Data in DB');
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error updating profile');
-  }
-};
-
-module.exports.deleteProfile = async (req, res) => {
-  try {
-    const { _id } = req.body;
-
-    await ProfileModel.findByIdAndDelete(_id);
-    res.send('Deleted Profile from DB');
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error deleting profile');
-  }
-};
+module.exports = ProfileController;
